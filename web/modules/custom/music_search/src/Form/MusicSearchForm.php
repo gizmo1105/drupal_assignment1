39482ca74209930e36a78f012f39d52d0f5bef8c
HAL <hallgrimur.j.jensson@me.com>
2024-12-10 14:07:37 +0000

    basic search form with radio buttons for type of search

--- web/modules/custom/music_search/src/Form/MusicSearchForm.php
+++ web/modules/custom/music_search/src/Form/MusicSearchForm.php
@@ -5,73 +5,69 @@ namespace Drupal\music_search\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-
-use Drupal\spotify_lookup\SpotifyLookupService;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 class MusicSearchForm extends FormBase {
-
-  protected SpotifyLookupService $spotifyService;
-
-  // Inject Spotify service
-  public function __construct(SpotifyLookupService $spotify_lookup_service) {
-    $this->spotifyService = $spotify_lookup_service;
-  }
-
-  public static function create(ContainerInterface $container): MusicSearchForm|static
-  {
-    return new static(
-      $container->get('music_search.spotify_lookup_service')
-    );
-  }
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId(): string
-  {
+  public function getFormId(): string {
     return 'music_search_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state): array
-  {
-    $form['track_name'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Track Name'),
-      '#description' => $this->t('Enter the name of the track you want to search for.'),
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['search_type'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Search Type'),
+      '#description' => $this->t('Choose what you want to search for.'),
+      '#options' => [
+        'artist' => $this->t('Artist'),
+        'song' => $this->t('Song'),
+        'album' => $this->t('Album'),
+      ],
+      '#default_value' => 'artist',
       '#required' => TRUE,
     ];
 
-    $form['search_results'] = [
-      '#markup' => $this->t('Search results will appear here.'),
+    $form['search_term'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search Term'),
+      '#description' => $this->t('Enter the term to search for.'),
+      '#required' => TRUE,
     ];
 
-    $form['#submit'][] = '::submitForm';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+    ];
+
+    $form['search_results'] = [
+      '#markup' => '<div id="search-results">' . $this->t('Search results will appear here after you submit the form.') . '</div>',
+    ];
+
     return $form;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state): void
-  {
-    $track_name = $form_state->getValue('track_name');
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    // Retrieve form values
+    $search_type = $form_state->getValue('search_type');
+    $search_term = $form_state->getValue('search_term');
 
-    // Use SpotifyLookupService to search for the track
-    $results = $this->spotifyService->searchTrack($track_name);
+    // Display the search type and term
+    drupal_set_message($this->t('You searched for a @type with the term: @term', [
+      '@type' => $search_type,
+      '@term' => $search_term,
+    ]));
 
-    // Prepare the results for display
-    $result_markup = '<ul>';
-    foreach ($results as $track) {
-      $result_markup .= '<li>' . $track['name'] . ' by ' . $track['artists'][0]['name'] . '</li>';
-    }
-    $result_markup .= '</ul>';
-
-    // Display the results in the form
-    $form['search_results']['#markup'] = $result_markup;
+    // (Optional) Update the results section dynamically (useful in AJAX scenarios)
+    $form['search_results']['#markup'] = $this->t('<p>Search Type: @type</p><p>Search Term: @term</p>', [
+      '@type' => $search_type,
+      '@term' => $search_term,
+    ]);
   }
 }
-
